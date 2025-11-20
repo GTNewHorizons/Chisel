20110c2b18c50cc1a322cc3aba86799d8aece6c1
danyadev <danyadev@mail.ru>
2025-11-20 20:29:46 +0300

    patch CTM renderer to allow rendering an overrided block texture as if it were a CTM texture

--- src/main/java/team/chisel/client/render/SubmapManagerVoidstone.java
+++ src/main/java/team/chisel/client/render/SubmapManagerVoidstone.java
@@ -25,66 +25,54 @@ public class SubmapManagerVoidstone extends SubmapManagerBase {
 
         @Override
         public void renderFaceXNeg(Block block, double x, double y, double z, IIcon icon) {
-            shiftBoundary(ForgeDirection.WEST);
             setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.WEST.ordinal()));
             super.renderFaceXNeg(block, x, y, z, null);
             clearOverrideBlockTexture();
-            resetBoundary(ForgeDirection.WEST);
 
             super.renderFaceXNeg(block, x, y, z, icon);
         }
 
         @Override
         public void renderFaceXPos(Block block, double x, double y, double z, IIcon icon) {
-            shiftBoundary(ForgeDirection.EAST);
             setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.EAST.ordinal()));
             super.renderFaceXPos(block, x, y, z, null);
             clearOverrideBlockTexture();
-            resetBoundary(ForgeDirection.EAST);
 
             super.renderFaceXPos(block, x, y, z, icon);
         }
 
         @Override
         public void renderFaceYNeg(Block block, double x, double y, double z, IIcon icon) {
-            shiftBoundary(ForgeDirection.DOWN);
             setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.DOWN.ordinal()));
             super.renderFaceYNeg(block, x, y, z, null);
             clearOverrideBlockTexture();
-            resetBoundary(ForgeDirection.DOWN);
 
             super.renderFaceYNeg(block, x, y, z, icon);
         }
 
         @Override
         public void renderFaceYPos(Block block, double x, double y, double z, IIcon icon) {
-            shiftBoundary(ForgeDirection.UP);
             setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.UP.ordinal()));
             super.renderFaceYPos(block, x, y, z, null);
             clearOverrideBlockTexture();
-            resetBoundary(ForgeDirection.UP);
 
             super.renderFaceYPos(block, x, y, z, icon);
         }
 
         @Override
         public void renderFaceZNeg(Block block, double x, double y, double z, IIcon icon) {
-            shiftBoundary(ForgeDirection.NORTH);
             setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.NORTH.ordinal()));
             super.renderFaceZNeg(block, x, y, z, null);
             clearOverrideBlockTexture();
-            resetBoundary(ForgeDirection.NORTH);
 
             super.renderFaceZNeg(block, x, y, z, icon);
         }
 
         @Override
         public void renderFaceZPos(Block block, double x, double y, double z, IIcon icon) {
-            shiftBoundary(ForgeDirection.SOUTH);
             setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.SOUTH.ordinal()));
             super.renderFaceZPos(block, x, y, z, null);
             clearOverrideBlockTexture();
-            resetBoundary(ForgeDirection.SOUTH);
 
             super.renderFaceZPos(block, x, y, z, icon);
         }
@@ -97,79 +85,6 @@ public class SubmapManagerVoidstone extends SubmapManagerBase {
                 MathHelper.floor_double(y),
                 MathHelper.floor_double(z),
                 side);
-        }
-
-        private void shiftBoundary(ForgeDirection direction) {
-            // When rendering a CTM overlay texture over a base block texture the base texture starts to z-fight.
-            // This method makes the base texture render slightly below the overlay.
-            // This submap check makes sure that we only apply this hack for blocks with CTM overlays
-            if (submap == null) {
-                return;
-            }
-
-            float epsilon = 0.0005F;
-
-            switch (direction) {
-                case WEST -> {
-                    renderMinX += epsilon;
-                    renderMinX += epsilon;
-                }
-                case EAST -> {
-                    renderMinX -= epsilon;
-                    renderMaxX -= epsilon;
-                }
-                case DOWN -> {
-                    renderMinY += epsilon;
-                    renderMaxY += epsilon;
-                }
-                case UP -> {
-                    renderMinY -= epsilon;
-                    renderMaxY -= epsilon;
-                }
-                case NORTH -> {
-                    renderMinZ += epsilon;
-                    renderMaxZ += epsilon;
-                }
-                case SOUTH -> {
-                    renderMinZ -= epsilon;
-                    renderMaxZ -= epsilon;
-                }
-            }
-        }
-
-        private void resetBoundary(ForgeDirection direction) {
-            if (submap == null) {
-                return;
-            }
-
-            float epsilon = 0.0005F;
-
-            switch (direction) {
-                case WEST -> {
-                    renderMinX -= epsilon;
-                    renderMaxX -= epsilon;
-                }
-                case EAST -> {
-                    renderMinX += epsilon;
-                    renderMaxX += epsilon;
-                }
-                case DOWN -> {
-                    renderMinY -= epsilon;
-                    renderMaxY -= epsilon;
-                }
-                case UP -> {
-                    renderMinY += epsilon;
-                    renderMaxY += epsilon;
-                }
-                case NORTH -> {
-                    renderMinZ -= epsilon;
-                    renderMaxZ -= epsilon;
-                }
-                case SOUTH -> {
-                    renderMinZ += epsilon;
-                    renderMaxZ += epsilon;
-                }
-            }
         }
     }
 
